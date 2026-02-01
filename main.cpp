547e8381a31cea0dbfbfc395a50673d2916b88e7
kiazh <kiashadow2007@gmail.com>
2026-02-01 18:00:23 -0500

    circle

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main ()
 {
     int width =  900;
     int height = 900;
+    Vector2 ballPos = {(float)450, (float)450};
 
     InitWindow (width, height, "burger");
 
@@ -15,6 +16,7 @@ int main ()
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        DrawCircleV(ballPos,50,RED);
         EndDrawing();
     }
     CloseWindow();
